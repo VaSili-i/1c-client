d325f2d57f3975003da545c88b5e02c09e6b65b5
Vasiliy <d1f9c9@gmail.com>
2024-11-04 09:20:25 +0800

    comit

--- config/webpack/getPlugins.ts
+++ config/webpack/getPlugins.ts
@@ -29,6 +29,7 @@ export function getPlugins({
   //   minRatio: 0.8,
   // });
 
+  console.log(apiUrl);
   const definePlugin = new webpack.DefinePlugin({
     __IS_DEV__: JSON.stringify(isDev),
     __API__: JSON.stringify(apiUrl)
